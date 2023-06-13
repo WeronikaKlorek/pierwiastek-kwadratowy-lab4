b317dcae5a0c7d553d944142dad02fd4eac9442b
weron <weron@WERONIKA>
2023-06-13 20:29:26 +0200

    Dodaj pliki projektów.

---
+++ Pierwiastek.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pierwiastek_kwadratowy
+{
+    public class Pierwiastek
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int c { get; set; }
+
+        public double obliczDelte(int a, int b, int c) {
+
+            double delta = 0;
+
+            // obliczanie delty na podstawie wzoru: D = b^2 - 4ac
+            delta = b * b - 4 * a * c;
+
+            return delta;
+        }
+
+        public void obliczPierwiastki(int a, int b, int c)
+        {
+            double delta = obliczDelte(a, b, c);
+            double x1, x2;
+
+
+            switch (delta)
+            {
+                case 0:
+                    x1 = x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+                    x2 = -b / 2 * a;
+                    Console.WriteLine($"Dla wpisanych parametrów delta przyjmuje wartość: {delta}\n" +
+                    $"Miejsce zerowe to: {Math.Round(x1, 2)}");
+                    break;
+                case > 0:
+                    x1 = (-b - Math.Sqrt(delta)) / 2 * a;
+                    x2 = (-b + Math.Sqrt(delta)) / 2 * a;
+                    Console.WriteLine($"Dla wpisanych parametrów delta przyjmuje wartość: {delta}\n" +
+                    $"Miejsca zerowa równania to: {Math.Round(x1, 2)} oraz {Math.Round(x2, 2)}");
+                    break;
+                case < 0:
+
+                    Console.WriteLine($"Delta miejsza od zera. Brak miejsc zerowych równania");
+                    break;
+
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using pierwiastek_kwadratowy;
+
+int a = 0;
+int b = 0;
+int c = 0;
+string input;
+
+Pierwiastek pierwiastek = new Pierwiastek();
+
+Console.WriteLine("Wprowadź wartość a dla równania:");
+
+
+while (a == 0)
+{
+    
+    a = int.Parse(Console.ReadLine());
+    if (a == 0)
+    {
+        Console.WriteLine("Nieprawidłowa wartość. Wprowadź liczbę ponownie. Wartość a nie może być zerem.");
+    }
+}
+
+Console.WriteLine("Wprowadź wartość b dla równania:");
+b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Wprowadź wartość c dla równania:");
+c = int.Parse(Console.ReadLine());
+
+pierwiastek.obliczPierwiastki(a, b, c);
+Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć...");
+Console.ReadKey();
